f7891ea70461d06a3fbae228611b440c714f3832
Nicholas Borba <nicholasborba@gmail.com>
2018-08-15 22:19:43 -0300

    Challenge: mergeStrings - Kotlin

---
+++ HackerRank/kotlinSolutions/src/Main.kt
@@ -0,0 +1,5 @@
+fun main(args : Array<String>){
+
+    println(merge("aaaa", "bbbbbbbbb"))
+
+}
---
+++ HackerRank/kotlinSolutions/src/mergeStrings.kt
@@ -0,0 +1,11 @@
+fun merge(a: String, b: String): String{
+    var result = ""
+    var larger = if (a.length > b.length) a.length else b.length
+
+    for (cont: Int in 0 until larger){
+        if (cont < a.length) result += a[cont]
+        if (cont < b.length) result += b[cont]
+    }
+
+    return result
+}
