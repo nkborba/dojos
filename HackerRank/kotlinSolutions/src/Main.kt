c83acb00230998bc6eeb5e4be11837b3d1d0a5f2
Nicholas Borba <nicholasborba@gmail.com>
2018-08-28 00:25:18 -0300

    New quickSort Try

--- HackerRank/kotlinSolutions/src/Main.kt
+++ HackerRank/kotlinSolutions/src/Main.kt
@@ -9,7 +9,8 @@ fun main(args : Array<String>){
     val nbSort = nicholasSort()
 
     //Input para função de ordenar vetor com BubbleSort
-    val vetor:IntArray = intArrayOf(654, 980, 50, 578, 65, 1, 800, 546, 48 ,87 ,5498, 75, 7, 789, 54, 8,4 ,78)
+    val vetor:IntArray = intArrayOf(654, 980, 50, 578, 65, 1)
+    val bigVetor:IntArray = intArrayOf(654, 980, 50, 578, 65, 1, 800, 546, 48 ,87 ,5498, 75, 7, 789, 54, 8,4 ,78)
     val vetorSimples:IntArray = intArrayOf(1,2,3,4,5,6,7,8,9,10)
 
 
@@ -20,9 +21,10 @@ fun main(args : Array<String>){
     //println(measureTimeMillis { println(bubbleSort.sort(vetor)) })
 
     //QuickSort WIP
-    //println(quick.quickSort(vetor, 0, vetor.size-1))
+    println(vetor.asList())
+    println(quick.quickSort(vetor, 0, vetor.size-1).asList())
 
     //Nicholas Borba Sort
-    println(measureTimeMillis { println(nbSort.sortHigh(vetor, 0))})
+    //println(measureTimeMillis { println(nbSort.sortHigh(vetor, 0))})
 
 }
--- HackerRank/kotlinSolutions/src/quickSort.kt
+++ HackerRank/kotlinSolutions/src/quickSort.kt
@@ -1,57 +1,42 @@
 import java.util.Collections.swap
 
 class QuickSort {
-    fun quickSort(vetor:IntArray, left:Int, right:Int): List<Int>{
+    fun quickSort(vetor:IntArray, left:Int, right:Int): IntArray{
 
-        //VETOR = (12, 98, 50, 578, 65, 48, 100)
+        //VETOR = (654, 980, 50, 578, 65, 1)
 
-        var pivot:Int; var pointEsq = left; var pointDir = right
-        var cont:Int = left
+        if (left == right) return vetor
 
-        if (pointDir-pointEsq <= 0){
-            println("estrou aqui")
-            return vetor.asList()
+        var pivot = vetor[left]
+        var cont1 = 0
+        var cont2 = 0
+
+        while (left+cont2 < right-cont1) {
+
+            while (cont1 < right - 1) {
+                if (pivot > vetor[right - cont1]) {
+                    vetor[right - cont1] = vetor[left+cont1].also { vetor[left+cont1] = vetor[right - cont1] }
+                    //println(vetor.asList())
+                    break
+                }
+                cont1++
+            }
+
+            while (cont2 < right - cont1) {
+                if (pivot < vetor[left + cont2]) {
+                    vetor[left + cont2] = vetor[right - cont1].also { vetor[right - cont1] = vetor[left + cont2] }
+                    //println(vetor.asList())
+                    break
+                }
+                cont2++
+            }
+
+            //println("Final:"+vetor.asList())
         }
 
+        println(right-cont1-1)
+        quickSort(vetor, left, right-cont1-1)
 
-        //for(i in 0 until vetor.size){
-        while (pointDir-pointEsq != 0) {
-
-            pivot = vetor[pointEsq]
-            println("Pivot: " + pivot)
-            println(vetor.asList())
-
-            //Enquanto ponteiro da direita for maior que o pivot, diminui a pos. do ponteiro
-            while (vetor[pointDir] > pivot) {
-                pointDir--
-            }
-
-            //println("Point Dir: "+vetor[pointDir])
-
-            //
-            if (vetor[pointDir] < pivot) {
-                vetor[pointEsq] = vetor[pointDir]
-                vetor[pointDir] = pivot
-            }
-
-            //Enquanto ponteiro da esquerda for menor que o pivot, aumenta a pos. do ponteiro
-            while (vetor[pointEsq] < pivot) {
-                pointEsq++
-            }
-
-            //
-            if (vetor[pointEsq] > pivot) {
-                vetor[pointDir] = vetor[pointEsq]
-                vetor[pointEsq] = pivot
-            }
-
-            println(pointEsq.toString() + " " + pointDir.toString())
-
-        }
-        cont++
-        quickSort(vetor, cont, vetor.size-1)
-        //}
-
-        return vetor.asList()
+        return vetor
     }
 }
